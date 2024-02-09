9689bcce32fff5a6447e9da4638beb3b4cb02460
Jakub Sopel <jakubsopel7@gmail.com>
2024-02-09 18:21:40 +0100

    feat: static files config, daisyui config

--- game_databse/settings.py
+++ game_databse/settings.py
@@ -127,8 +127,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+print(BASE_DIR)
+STATIC_ROOT = '/static/'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'game_databse/static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
