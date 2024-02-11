4faf78647283ad4d18f2cd2379d34e49c5ac17ef
Sam Sepiol <jakubsopel7@gmail.com>
2024-02-11 18:07:15 +0100

    feat: login form, new navbar, login view

--- game_databse/apps/accounts/views.py
+++ game_databse/apps/accounts/views.py
@@ -1,5 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
 
 
 class LoginView(TemplateView):
     template_name = 'login.html'
+
+    def post(self, request, *args, **kwargs):
+        print(request)
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(redirect_to='/games')
+        else:
+            return HttpResponseRedirect(redirect_to='/accounts/login')
